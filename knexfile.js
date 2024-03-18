f0d9746983d660738ddc6c2c432fcb84d8a2e9f3
Jonathan Samael <jonathansamael@live.com>
2024-03-18 13:08:07 -0300

    "fix db host"

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ module.exports = {
      client: 'mysql2',
      connection: {
           // host: '127.0.0.1',
-          host: 'https://conectvaquejada.com.br/',
+          host: 'https://auth-db1295.hstgr.io/index.php?route=/database/structure&db=u709235075_conect',
           user: 'root',
           password: 'ubr23celestino:J',
           database: 'u709235075_conect'
