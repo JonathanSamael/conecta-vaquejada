9b93a41265f28b98f423f2f7f2192c307f1e04bb
Jonathan Samael <jonathansamael@live.com>
2024-03-18 14:04:03 -0300

    "test new route to fix error 500 server"

--- knexfile.js
+++ knexfile.js
@@ -2,12 +2,11 @@ module.exports = {
      client: 'mysql2',
      connection: {
           // host: '127.0.0.1',
-          host: '193.203.166.166',
+          host: 'srv1295.hstgr.io',
           user: 'root',
           password: 'ubr23celestino:J',
           database: 'u709235075_conect'
-          // user: 'root',
           // password: '1a2b3c4d@S',
-          // database: 'conectvaquejadas'
+          // database: 'conectvaquejada'
      }
 };
