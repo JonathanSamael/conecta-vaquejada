73908d617d181b0af86760b2b98e975f7807d3cd
Jonathan Samael <jonathansamael@live.com>
2024-03-18 15:32:51 -0300

    "upgrade: configure gitignore && test env. variables"

--- knexfile.js
+++ knexfile.js
@@ -2,10 +2,10 @@ module.exports = {
      client: 'mysql2',
      connection: {
           // host: '127.0.0.1',
-          host: HOST,
-          user: USER,
-          password: CLIENT_KEY,
-          database: HOST_DB
+          host: process.env.HOST,
+          user: process.env.USER,
+          password: process.env.CLIENT_KEY,
+          database: process.env.HOST_DB
           // password: '1a2b3c4d@S',
           // database: 'conectvaquejada'
      }
