2a24ea9f067ce7ac4ed39ea52ed24b71491fcdbf
Jonathan Samael <jonathansamael@live.com>
2024-03-18 12:58:47 -0300

    "feat - update database infos"

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,13 @@
 module.exports = {
      client: 'mysql2',
      connection: {
-          host: '127.0.0.1',
-          // host: 'https://connect-vaqueja.vercel.app',
+          // host: '127.0.0.1',
+          host: 'https://conectvaquejada.com.br/',
           user: 'root',
-          password: '1a2b3c4d@S',
-          database: 'conectvaquejada'
+          password: 'ubr23celestino:J',
+          database: 'u709235075_conect'
+          // user: 'root',
+          // password: '1a2b3c4d@S',
+          // database: 'conectvaquejadas'
      }
 };
