e693ef3b537c66a419e72ae68f51646116fa7fb3
Jonathan Samael <jonathansamael@live.com>
2024-03-19 16:32:43 -0300

    "fix: variables to knex"

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,13 @@
 module.exports = {
      client: 'mysql2',
      connection: {
-          host: '127.0.0.1',
-          user: 'root',
-          // host: process.env.HOST,
-          // user: process.env.USER,
-          // password: process.env.CLIENT_KEY,
-          // database: process.env.HOST_DB
-          password: '1a2b3c4d@S',
-          database: 'conectvaquejada'
+          host: process.env.HOST,
+          user: process.env.USER,
+          password: process.env.CLIENT_KEY,
+          database: process.env.HOST_DB
+          // host: '127.0.0.1',
+          // user: 'root',
+          // password: 'senha',
+          // database: 'conectvaquejada'
      }
 };
