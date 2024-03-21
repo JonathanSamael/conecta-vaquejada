b33e07d48103a7f9527e488be71aa400578e375b
Júlio Elias de Sousa rocha <julioeliasdesousarocha.work@gmail.com>
2024-03-21 07:18:49 -0300

    data correction in knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,9 @@
 module.exports = {
      client: 'mysql2',
      connection: {
-          // host: process.env.HOST,
-          // user: process.env.USER,
-          // password: process.env.CLIENT_KEY,
-          // database: process.env.HOST_DB
-
-          host: '127.0.0.1',
-          user: 'root',
-          password: 'senha',
-          database: 'conectvaquejada'
+          host: process.env.HOST,
+          user: process.env.USER,
+          password: process.env.CLIENT_KEY,
+          database: process.env.HOST_DB
      }
 };
