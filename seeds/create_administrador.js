b7197fd7d615eb28291a32e98505300bc984dd73
Jonathan Samael <jonathansamael@live.com>
2024-03-25 13:35:30 -0300

    "update: include new encrypt to db"

--- seeds/create_administrador.js
+++ seeds/create_administrador.js
@@ -4,11 +4,12 @@
 */
 
 const bcrypt = require('bcrypt');
+const senha = process.env.SENHA_HASH;
 
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex('administradores').del()
   await knex('administradores').insert([
-    { cpf: '123.456.789-00', senha: bcrypt.hashSync('123456', 10) },
+    { cpf: '123.456.789-00', senha: sha1(senha)},
   ]);
 };
