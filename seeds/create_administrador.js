38da7ed044e2a7d963b499884c2606a3502ec7ad
Júlio Elias de Sousa rocha <julioeliasdesousarocha.work@gmail.com>
2024-04-02 09:44:55 -0300

    validation of the vaquejada request form

--- seeds/create_administrador.js
+++ seeds/create_administrador.js
@@ -6,7 +6,7 @@ exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex('administradores').del()
   await knex('administradores').insert([
-    { cpf: '123.456.789-00', senha: ('123456') }
+    { cpf: '123.456.789-10', senha: ('123456') }
 
   ]);
 };
